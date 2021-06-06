f3f589ea6174ac345f089f913b68bc18bcc6b3f8
Revin Tanri <tanrirevin@gmail.com>
2021-06-06 13:22:39 +0800

    Fix carousel formating

--- controllers/products_controller.js
+++ controllers/products_controller.js
@@ -1,27 +1,45 @@
-const { customers, Posting } = require("../models/post_model");
+const { PostModel } = require("../models/post_model");
+const moment = require("moment");
+// const { customers, Posting } = require("../models/post_model");
 
 module.exports = {
   index: (req, res) => {
-    res.render("products/index", { customers });
+    res.render("products/index");
   },
-  customers: (req, res) => {
-    res.render("products/customers", { customers });
+  customers: async (req, res) => {
+    let postings = [];
+    try {
+      postings = await PostModel.find();
+    } catch (err) {
+      res.statusCode(500);
+      return "Server error 500";
+    }
+    res.render("products/customers", {
+      postings: postings,
+    });
   },
   newPost: (req, res) => {
     res.render("products/new");
-    console.log(1);
   },
   createPost: (req, res) => {
     console.log(req.body);
-    const newEntry = new Posting(
-      req.body.customerName,
-      req.body.serviceName,
-      req.body.category,
-      req.body.image,
-      customers.length
-    );
-    customers.push(newEntry);
-    res.redirect("/beautylash/customers");
+
+    const timestampNow = moment().utc();
+
+    PostModel.create({
+      name: req.body.name,
+      service: req.body.name,
+      category: req.body.category,
+      image: req.body.image,
+      created_at: timestampNow,
+    })
+      .then(createResp => {
+        res.redirect("/beautylash/customers");
+      })
+      .catch(err => {
+        console.log(err);
+        res.redirect("/beautylash/add-post");
+      });
   },
 
   homepage: (req, res) => {
